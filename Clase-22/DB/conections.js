74f8db7a25f105010b448a3e7f7e1424e5630627
LeoMontani <leonel.montani1@gmail.com>
2023-08-29 21:05:16 -0300

    Clase 22 agregada

---
+++ Clase-22/DB/conections.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL)
+  console.log("Conectado a la base de datos");
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    disconnectDB();
+    console.log("Desconectado de la base de datos");
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports =  {
+  connectDB,
+  disconnectDB
+}
